36fa6520e93e8c66734bf4bb8c7154dfaaad21fe
joao-manoel <joaomanoel.ms.br@gmail.com>
2019-08-09 21:57:06 -0400

    port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const server = express()
 
 server.use(express.json())
 
-const port = 8080
+const port = 80
 
 server.listen(port, () =>{
   console.log("[Server]", `Backend is running on port ${port}.`)
