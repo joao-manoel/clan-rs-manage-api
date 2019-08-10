6ef95a071d5c56fc772ed6deeff52d1c5eb3656d
joao-manoel <joaomanoel.ms.br@gmail.com>
2019-08-10 11:58:39 -0400

    arrumando bug

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect(`mongodb+srv://${procss.env.DB_USER}:${procss.env.DB_PASS}@cluster0-lmnna.mongodb.net/members?retryWrites=true&w=majority`,  { useNewUrlParser: true })
+module.exports = mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-lmnna.mongodb.net/members?retryWrites=true&w=majority`,  { useNewUrlParser: true })
