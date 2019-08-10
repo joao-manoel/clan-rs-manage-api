94d49f4abb40884c653c00adc98fa21a9ad045c1
joao-manoel <joaomanoel.ms.br@gmail.com>
2019-08-10 11:55:25 -0400

    adicionado novas chaves env

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb+srv://bot:moraes145@cluster0-lmnna.mongodb.net/members?retryWrites=true&w=majority',  { useNewUrlParser: true })
+module.exports = mongoose.connect(`mongodb+srv://${procss.env.DB_USER}:${procss.env.DB_PASS}@cluster0-lmnna.mongodb.net/members?retryWrites=true&w=majority`,  { useNewUrlParser: true })
