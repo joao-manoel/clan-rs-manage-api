578f0f539299251b2338f2160bc115fcae5339c7
joao-manoel <joaomanoel.ms.br@gmail.com>
2019-08-19 23:28:28 -0400

    novas frases

--- src/bot/commands/match.js
+++ src/bot/commands/match.js
@@ -3,55 +3,67 @@ const axios = require('axios')
 const utils = require('../../utils/ultis.js')
 
 exports.run = async (client, message, args) => {
-  
-  var membros = await axios.get(`${process.env.API_URL}/api/members`)
-  var membros = membros.data
 
-  var membro1 = membros[Math.floor(Math.random() * membros.length) + 1]
-  var membro2 = membros[Math.floor(Math.random() * membros.length) + 1]
+  if (args.length >= 2) {
 
-  
-  var frases = [
-    'Hoje o trofeu de casal do ano vai para...', 
-    'Que casal lindo!', 
-    'Todos ja sabiam que era amor!']
-  var frases_rand = frases[Math.floor(Math.random() * frases.length)]
+    message.channel.send('Em breve voces poderam shipar um casal, @manoel ta ta trabalhando nisso!')
 
-  var emoji = [
-    '😍',
-    '💑',
-    '❤️',
-    '🧡',
-    '💛',
-    '💚',
-    '💙',
-    '💜',
-    '🖤',
-    '💞',
-    '💓',
-    '💝'
-  ]
+  } else {
 
-  var emoji_rand =emoji[Math.floor(Math.random() * emoji.length)]
+    var membros = await axios.get(`${process.env.API_URL}/api/members`)
+    var membros = membros.data
 
-  var cards = {
-    color: 0xf556ed,
-    author: {
-      name: message.member.user.username,
-      icon_url: message.member.user.avatarURL
-    },
-    fields: [
-      {
-        name: `${frases_rand}`,
-        value: `***${utils.convertName(membro1.name)} ${emoji_rand} ${utils.convertName(membro2.name)}***`
-      }
-    ],
+    var membro1 = membros[Math.floor(Math.random() * membros.length) + 1]
+    var membro2 = membros[Math.floor(Math.random() * membros.length) + 1]
+
+
+    var frases = [
+      'Hoje o trofeu de casal do ano vai para...',
+      'Que casal lindo!',
+      'Todos ja sabiam que era amor!',
+      'Vamos aplaudir de pe o casal!',
+      'Isso que e um casal da pohh@@!',
+      'Fico pensando quem é a voz ativa do casal?!',
+      'Nasceram um para o outro!'
+    ]
+    var frases_rand = frases[Math.floor(Math.random() * frases.length)]
+
+    var emoji = [
+      '😍',
+      '💑',
+      '❤️',
+      '🧡',
+      '💛',
+      '💚',
+      '💙',
+      '💜',
+      '🖤',
+      '💞',
+      '💓',
+      '💝'
+    ]
+
+    var emoji_rand = emoji[Math.floor(Math.random() * emoji.length)]
+
+    var cards = {
+      color: 0xf556ed,
+      author: {
+        name: message.member.user.username,
+        icon_url: message.member.user.avatarURL
+      },
+      fields: [
+        {
+          name: `${frases_rand}`,
+          value: `***${utils.convertName(membro1.name)} ${emoji_rand} ${utils.convertName(membro2.name)}***`
+        }
+      ],
       footer: {
         text: '❤️Desenvolvido por @Manoel.'
       }
-  }
+    }
 
-  message.channel.send({embed: cards})
+    message.channel.send({ embed: cards })
+  }
 
 }
 exports.help = {
