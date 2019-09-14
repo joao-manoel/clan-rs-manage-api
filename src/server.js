ac8aa3ca9671630391d79e268d3cffe09689c2be
joao-manoel <joaomanoel.ms.br@gmail.com>
2019-09-14 03:35:17 -0400

    s

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ server.use(express.json())
 const port = process.env.PORT || 3333
 
 server.listen(port, () =>{
-  console.log("[Server]", `Backend is running on port ${port}.`)
+  console.log("[Servidor]", `Backend is running on port ${port}.`)
 }, (err) =>{
   if(err) console.log(err)
 })
