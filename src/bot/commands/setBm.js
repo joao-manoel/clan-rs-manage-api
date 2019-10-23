83458d2315af4a69139d22f0d65c55e4e7ff0d6d
joao-manoel <joaomanoel.ms.br@gmail.com>
2019-10-23 17:22:44 -0400

    update setbm

--- src/bot/commands/setBm.js
+++ src/bot/commands/setBm.js
@@ -4,11 +4,11 @@ axios = require('axios')
 exports.run = async (client, message, args) => {
   
   if(utils.hasPermissionAll(message, 2)){
-    if(args[0] = 'ativar'){
+    if(args[0] == 'on'){
       let update = await axios.post(`${process.env.API_URL}/api/notification/bm/1`)
       message.channel.send(`Anuncio ativado! \n proximo anuncio sera ás ${process.env.HOURS}:${process.env.MINUTES}`)
       
-    }else if(args[0] = 'desativar'){
+    }else if(args[0] == 'off'){
       let update = await axios.post(`${process.env.API_URL}/api/notification/bm/0`)
       message.channel.send(`Anuncio desativado! \n proximo anuncio sera daqui 1 reset`)
     }
