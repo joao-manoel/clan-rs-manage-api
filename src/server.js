9ec909b7c5320fb6b2c719107dacc2fbc8dd46a4
João Manoel <joaomanoel.ms.br@gmail.com>
2019-12-19 17:54:48 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require("express")
 require('./config/database')
-
 const server = express()
+
+server.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTION, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  next()
+})
 
 server.use(express.json())
 
